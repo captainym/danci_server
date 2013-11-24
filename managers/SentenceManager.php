d89ed5a4b1856a4c9ab19c37f4c7a57454233390
captainym <captainyuming@gmail.com>
2013-11-24 23:34:38 +0800

    add bae server url and mcryt

--- managers/SentenceManager.php
+++ managers/SentenceManager.php
@@ -24,9 +24,9 @@ class SentenceManager extends Manager {
                 $voice_key = $item['voice_key'];
 
                 $mp3_file_path = $base_path . "/" . $word . "/sentence/" . $voice_key;
-                $mp3_url = $base_url . "?file_type=mp3&file_path=".urlencode($mp3_file_path);
+//                $mp3_url = $base_url . "?file_type=mp3&file_path=".urlencode($mp3_file_path);
                 unset($item['voice_key']);
-                $item['voice'] = $mp3_url;
+                $item['voice'] = $this->gen_download_url('mp3', $mp3_file_path);
                 $rs []= $item;
             }
         }
@@ -35,7 +35,34 @@ class SentenceManager extends Manager {
         return $rs;
     }
 
+    /**
+     * 获取可用的bae下载server
+     * @return string
+     */
     public function get_download_server_url() {
-        return "http://imgdu.com/bdc/cloud/file";
+        return "http://detail.duapp.com";
+    }
+
+    /**
+     * 获取一个最空的bcs
+     */
+    public function get_download_bcs() {
+        $bucket = G::$conf['bdc']['BUCKET'];
+        $baidu_ak = G::$conf['bdc']['BAIDU_AK'];
+        $baidu_sk = G::$conf['bdc']['BAIDU_SK'];
+        $params = array('vender'=>'baidu','bucket'=>$bucket,
+            'ak'=>$baidu_ak, 'sk'=>$baidu_sk);
+
+        return $params;
+    }
+    public function gen_download_url($file_type, $file_path) {
+        $base_url = $this->get_download_server_url();
+        $bcs_token_params = $this->get_download_bcs();
+        $bcs_token_params['file_path'] = $file_path;
+        $bcs_token_params['file_type'] = $file_type;
+
+        $token = $this->mCrypt->encrypt(json_encode($bcs_token_params));
+
+        return $base_url . "?token=" . urlencode($token);
     }
 }
